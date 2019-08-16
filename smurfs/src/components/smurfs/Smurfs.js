89d8f2bf0d0cbb09d1019e9e3b694ba625c2dbcf
RealWillBrooks <wbrooks107@gmail.com>
2019-08-16 13:57:32 -0500

    Questions answered, MVP.

---
+++ smurfs/src/components/smurfs/Smurfs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { removeSmurf } from "../store/action";
+
+function Smurfs(props) {
+  return (
+    <div className='existing-smurf'>
+        <h3>Name: {props.smurf.name}</h3>
+        <h4>{props.smurf.age} years old and {props.smurf.height}cm tall</h4>
+        <button className='delete' onClick={removeSmurf}>Delete</button>
+    </div>
+  );
+}
+export default Smurfs
---
+++ smurfs/src/components/store/reducer.js
@@ -0,0 +1,69 @@
+import {
+    FETCH_START,
+    FETCH_SUCCESS,
+    FETCH_FAIL,
+    ADD_START,
+    ADD_SUCCESS,
+    ADD_FAIL,
+    REMOVE_SMURF
+  } from "./action";
+  
+  const initialState = {
+    smurfs: [],
+    err: "",
+    isFetching: false
+  };
+  
+  export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case FETCH_START:
+        return {
+          ...state,
+          err: "",
+          isFetching: true,
+          smurfs: []
+        };
+      case FETCH_SUCCESS:
+        return {
+          ...state,
+          err: "",
+          isFetching: false,
+          smurfs: action.payload
+        };
+      case FETCH_FAIL:
+        return {
+          ...state,
+          err: action.payload,
+          isFetching: false,
+          smurfs: []
+        };
+      case ADD_START:
+        return {
+          ...state,
+          err: "",
+          isFetching: true,
+          smurfs: []
+        };
+      case ADD_SUCCESS:
+        return {
+          ...state,
+          err: "",
+          isFetching: false,
+          smurfs: action.payload
+        };
+      case ADD_FAIL:
+        return {
+          ...state,
+          err: action.payload,
+          isFetching: false,
+          smurfs: []
+        };
+      case REMOVE_SMURF:
+          return{
+          ...state.filter((data, i) => i !== action.id)
+          }
+  
+      default:
+        return state;
+    }
+  };
